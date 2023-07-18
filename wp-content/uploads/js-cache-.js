9f0472ee65ab68e2747e2e0e16855348bd2f4410
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-07-18 15:11:08 +0300

    Add files via upload

---
+++ wp-content/uploads/js-cache-.js
@@ -0,0 +1,13 @@
+    (function($){
+    $(document).ready(function() {
+
+        
+        $('.parallax').each(function(){
+           $(this).parallax("50%", 0.4);
+        });
+
+        
+        
+    });
+    })(jQuery);
+     /*2021-06-08 07:28*//* JS Generator Execution Time: 2.0980834960938E-5 seconds */
